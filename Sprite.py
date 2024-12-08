8d7104ef8cdeb677ddf74ee8b88eaac1ecdb2f30
mr.boombastik31 <timchik.oleksandr@gmail.com>
2024-12-08 18:49:50 +0200

    added comments, fixed rotation

--- Sprite.py
+++ Sprite.py
@@ -9,29 +9,39 @@ class Sprite(pygame.sprite.Sprite):
         self.size = self.image.get_size()
         self.rect.x = x
         self.rect.y = y
-        self.direction = 0
+        self.rotation = 0
 
+    # Малювання об'єкту у вікні
     def draw(self, window):
         window.blit(self.image, self.rect)
 
+    # Отримання поточної позиції
     def get_pos(self):
         return self.rect.x, self.rect.y
 
+    # встановлення позиції
     def set_pos(self, x, y):
         self.rect.x = x
         self.rect.y = y
 
+    # Встановлення нової позиції через метод set_pos
     def move(self, x, y):
         self.set_pos(self.rect.x + x, self.rect.y + y)
 
+
+    # Перевірка колізії
     def check_collision(self, sprite2):
         return sprite2.rect.colliderect(self.rect)
 
+    # повороти
     def rotate(self, angle):
-        self.image = pygame.transform.rotate(self.image, angle)
+        self.rotation += angle # Змінюємо значення атрибуту rotation на значення кута
+        self.image = pygame.transform.rotate(self.image, angle) # Обертаємо наше зображення на цей самий кут
 
-    def set_rotation(self, value):
-        self.direction = value
+    def set_rotation(self, angle):
+        self.image = pygame.transform.rotate(self.image, angle - self.rotation) # Зміна поточного напрямку
+        self.rotation = angle
+
 
 
 
--- main.py
+++ main.py
@@ -1,65 +1,60 @@
 import pygame as pg
 from Tank import Tank
-from enum import Enum
 import sys
-
-class Direction(Enum):
-    UP = 0
-    RIGHT = -90
-    LEFT = 90
-    DOWN = 180
 
 pg.init()
 
-window = pg.display.set_mode((500, 500))
+window = pg.display.set_mode((500, 500)) #Встановлюю розмір вікна
 
-WIDTH, HEIGHT = pg.display.get_surface().get_size()
+WIDTH, HEIGHT = pg.display.get_surface().get_size()   #Встановлюю фізичні межі вікна
 
 done = False
 
-tank = Tank('tank_blue.jpg', 100, 100, 3.5)
-
+# Створюю основні об'єкти і закидую їх в групу
+tank = Tank('tank_blue.jpg', 100, 100, 2)
+#bullet = Bullet('bulletBlue1.png', 200, 150, 3.5)
 group = pg.sprite.Group()
 group.add(tank)
+#group.add(bullet)
 
+# Клок для ФПС
 clock = pg.time.Clock()
-
-last_direction = 0
 
 while not done:
 
-    window.fill((80, 80, 80))
+    window.fill((80, 80, 80)) #Заповнюю вікно сірим кольором
+    key = pg.key.get_pressed() #direction - зчитує натиснуту клавішу
 
+
+    if key[pg.K_RIGHT] and tank.rect.right < WIDTH:
+        tank.move(tank.speed, 0)
+        tank.set_rotation(90)
+
+    if key[pg.K_LEFT] and tank.rect.left > 0:
+        tank.move(-tank.speed, 0)
+        tank.set_rotation(-90)
+
+    if key[pg.K_UP] and tank.rect.top > 0:
+        tank.move(0, -tank.speed)
+        tank.set_rotation(180)
+
+    if key[pg.K_DOWN] and tank.rect.bottom < HEIGHT:
+        tank.move(0, tank.speed)
+        tank.set_rotation(0)
+
+
+    # Малюю всі об'єкти, що знахдяться в групі
     group.draw(window)
 
+    # Event-Handler (зчитує всі івенти (нажаття клавіш і тд) один за цикл)
     for event in pg.event.get():
         if event.type == pg.QUIT:
             done = False
 
-
-    direction = pg.key.get_pressed()
-
-    if direction[pg.K_RIGHT] and tank.rect.right < WIDTH:
-        tank.move(tank.speed, 0)
-        tank.set_rotation(-90)
-
-    if direction[pg.K_LEFT] and tank.rect.left > 0:
-        tank.move(-tank.speed, 0)
-        tank.set_rotation(90)
-
-    if direction[pg.K_UP] and tank.rect.top > 0:
-        tank.move(0, -tank.speed)
-        tank.set_rotation(0)
-
-    if direction[pg.K_DOWN] and tank.rect.bottom < HEIGHT:
-        tank.move(0, tank.speed)
-        tank.set_rotation(180)
-
-
     clock.tick(120)
 
+    # Перемальовка кадру
     pg.display.update()
-
 
 pg.quit()
 sys.exit()
