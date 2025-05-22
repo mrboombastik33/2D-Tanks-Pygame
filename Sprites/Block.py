bb08ffd117c5bc868d2946e12c55bc48da33e3d9
mrboombastik33 <timchik.oleksandr@gmail.com>
2025-05-22 15:51:10 +0300

    added documentation for block

--- Sprites/Block.py
+++ Sprites/Block.py
@@ -2,15 +2,14 @@ import pygame
 from Sprites.Sprite import Sprite
 from additional.settings import *
 
+
+#Цей клас описує основні атрибути, які має блок (включно з тим чи може він бути знищений)
 class Block(Sprite):
     def __init__(self, image_link, x, y, breakable):
         super().__init__(image_link, x, y)
         self.image = pygame.transform.scale_by(self.image, SCALING_BLOCKS)
         self.rect = self.rect.scale_by(SCALING_BLOCKS)
         self.breakable = breakable
-
-    # def __str__(self):
-    #     return f"Block(type={self.block_type}, position=({self.rect.x}, {self.rect.y}))"
 
 
 
